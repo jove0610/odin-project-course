6dcf1595855f622dfb4182685f1c063a6261dd04
jove0610 <jove0610@gmail.com>
2025-12-31 11:20:00 +0800

    Rock Paper Scissors: allow playing rps game on UI

--- projects/rock-paper-scissors/script.js
+++ projects/rock-paper-scissors/script.js
@@ -1,3 +1,8 @@
+const btnContainer = document.querySelector('.btn-container')
+const gameResultText = document.querySelector('.game-result-text')
+const playerScoreText = document.querySelector('.player-score')
+const computerScoreText = document.querySelector('.computer-score')
+
 function getComputerChoice() {
     // Create variable choice
     let choice
@@ -23,51 +28,60 @@ function getComputerChoice() {
     return choice
 }
 
-function getHumanChoice() {
-    const choice = prompt('Select between "rock", "paper" or "scissors"')
-    return choice.toLowerCase()
-}
-
-function playGame() {
-    let humanScore = 0
-    let computerScore = 0
-
-    const playRound = function(humanChoice, computerChoice) {
-        // handle draw scenario
-        if (humanChoice === computerChoice) {
-            console.log("It's a draw!")
-            return
-        }
-
-        // IF human is rock AND computer is scissors
-        // OR IF human is paper AND computer is rock
-        // OR IF human is scissors and computer is paper
-        if ((humanChoice === 'rock' && computerChoice === 'scissors') ||
-            (humanChoice === 'paper' && computerChoice === 'rock') ||
-            (humanChoice === 'scissors' && computerChoice === 'paper')
-        ) {
-            // handle win scenario
-            humanScore++
-            console.log(`You win! ${humanChoice} beats ${computerChoice}`)
-            return
-        }
-        // ELSE handle lose scenario
-        computerScore++
-        console.log(`You lose! ${humanChoice} loses to ${computerChoice}`)
+function getHumanChoice(buttonClass) {
+    if (buttonClass === 'rock-btn') {
+         return 'rock'
+    } else if (buttonClass === 'paper-btn') {
+         return 'paper'
+    } if (buttonClass === 'scissors-btn') {
+         return 'scissors'
     }
-
-    playRound(getHumanChoice(), getComputerChoice())
-    playRound(getHumanChoice(), getComputerChoice())
-    playRound(getHumanChoice(), getComputerChoice())
-    playRound(getHumanChoice(), getComputerChoice())
-    playRound(getHumanChoice(), getComputerChoice())
-
-    console.log('==================')
-    console.log('Final Score:')
-    console.log('Player: ', humanScore)
-    console.log('Computer: ', computerScore)
-
-    alert('Game Over! Open devtools console to view results!')
 }
 
-playGame()
+function playRound(humanChoice, computerChoice) {
+    // handle draw scenario
+    if (humanChoice === computerChoice) {
+        gameResultText.textContent = "It's a draw!"
+    // IF human is rock AND computer is scissors
+    // OR IF human is paper AND computer is rock
+    // OR IF human is scissors and computer is paper
+    } else if ((humanChoice === 'rock' && computerChoice === 'scissors') ||
+        (humanChoice === 'paper' && computerChoice === 'rock') ||
+        (humanChoice === 'scissors' && computerChoice === 'paper')
+    ) {
+        // handle win scenario
+        let humanScore = playerScoreText.textContent
+        humanScore++
+        playerScoreText.textContent = humanScore
+        gameResultText.textContent = `You win! ${humanChoice} beats ${computerChoice}`
+    // ELSE handle lose scenario
+    } else {
+        let computerScore = computerScoreText.textContent
+        computerScore++
+        computerScoreText.textContent = computerScore
+        gameResultText.textContent = `You lose! ${humanChoice} loses to ${computerChoice}`
+    }
+}
+
+btnContainer.addEventListener('click', (e) => {
+    const clickedBtnClass = e.target.classList[0]
+    if (!['rock-btn', 'paper-btn', 'scissors-btn'].includes(clickedBtnClass)) {
+        return
+    }
+    
+    const humanChoice = getHumanChoice(clickedBtnClass);
+    playRound(humanChoice, getComputerChoice())
+
+    const humanScore = +playerScoreText.textContent
+    const computerScore = +computerScoreText.textContent
+    if (humanScore === 5 || computerScore === 5) {
+        const result = humanScore === 5 ? 'win' : 'lose'
+        const gameOver = document.createElement('h3')
+        gameOver.textContent = `Game Over! You ${result}...`
+        gameResultText.parentNode.appendChild(gameOver)
+
+        document.querySelector('.rock-btn').setAttribute('disabled', '')
+        document.querySelector('.paper-btn').setAttribute('disabled', '')
+        document.querySelector('.scissors-btn').setAttribute('disabled', '')
+    }
+})
