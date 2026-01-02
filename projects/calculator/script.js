600ab33737b93cb06ec7a13446bada5e178560da
jove0610 <jove0610@gmail.com>
2026-01-02 23:55:08 +0800

    Calculator: handle displaying of digits

--- projects/calculator/script.js
+++ projects/calculator/script.js
@@ -2,12 +2,21 @@ const ADDITION_OPERATOR = '+'
 const SUBTRACTION_OPERATOR = '-'
 const MULTIPLICATION_OPERATOR = 'x'
 const DIVISION_OPERATOR = '/'
+const ARITHMETIC_OPERATOR = [
+    ADDITION_OPERATOR,
+    SUBTRACTION_OPERATOR,
+    MULTIPLICATION_OPERATOR,
+    DIVISION_OPERATOR,
+]
 
 const displayEl = document.querySelector('.display')
+const digitEls = document.querySelectorAll('.digit')
+const operatorEls = document.querySelectorAll('.operator')
 
 let operand1 = '0'
 let operand2 = null
 let operator = null
+let display = operand1 // initial display is operand1
 
 function add(num1, num2) {
     return Number(num1) + Number(num2)
@@ -46,6 +55,33 @@ function operate(operator, num1, num2) {
     return answer
 }
 
+function populateDisplay(number) {
+    display = number
+    displayEl.textContent = Number(display).toLocaleString()
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     displayEl.textContent = operand1
 })
+
+digitEls.forEach(el => el.addEventListener('click', (e) => {
+    const digit = e.target.textContent
+    
+    if (operator === null) {
+        operand1 = operand1 === '0'
+            ? digit
+            : operand1 + digit
+        populateDisplay(operand1)
+    } else if (ARITHMETIC_OPERATOR.includes(operator)) {
+        operand2 = ['0', null].includes(operand2)
+            ? digit
+            : operand2 + digit
+        populateDisplay(operand2)
+    }
+}))
+
+operatorEls.forEach(el => el.addEventListener('click', (e) => {
+    operator = e.target.textContent
+    document.querySelector('.highlight')?.classList.remove('highlight')
+    e.target.classList.add('highlight')
+}))
