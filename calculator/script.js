32be90dc3a0ff70bde6fe247648f4e330486bbda
jove0610 <jove0610@users.noreply.github.com>
2026-01-02 18:49:12 +0000

    deploy: e882d920173a82ced7edfb4bdd85e5faebc38658

--- calculator/script.js
+++ calculator/script.js
@@ -1,138 +1,160 @@
 const ADDITION_OPERATOR = '+'
-const SUBTRACT_OPERATOR = '-'
-const MULTIPLY_OPERATOR = 'x'
+const SUBTRACTION_OPERATOR = '-'
+const MULTIPLICATION_OPERATOR = 'x'
 const DIVISION_OPERATOR = '/'
 const EQUALS_OPERATOR = '='
 const MATH_ERROR = 'Math error...'
-const ARITMETIC_OPERATIONS = [
+const ARITHMETIC_OPERATOR = [
     ADDITION_OPERATOR,
-    SUBTRACT_OPERATOR,
-    MULTIPLY_OPERATOR,
+    SUBTRACTION_OPERATOR,
+    MULTIPLICATION_OPERATOR,
     DIVISION_OPERATOR,
 ]
 
+const allClearEl = document.querySelector('.allclear')
+const decimalEl = document.querySelector('.decimal')
 const displayEl = document.querySelector('.display')
+const equalsEl = document.querySelector('.equals')
+const digitEls = document.querySelectorAll('.digit')
+const operatorEls = document.querySelectorAll('.operator')
 
-let display = '0'
 let operand1 = '0'
-let operand2 = ''
+let operand2 = null
 let operator = null
+let display = operand1 // initial display is operand1
 
 function operate(operator, num1, num2) {
     let answer
     switch(operator) {
         case ADDITION_OPERATOR:
-            answer = num1 + num2
-            break;
-        case SUBTRACT_OPERATOR:
+            answer = Number(num1) + Number(num2)
+            break
+        case SUBTRACTION_OPERATOR:
             answer = num1 - num2
-            break;
-        case MULTIPLY_OPERATOR:
+            break
+        case MULTIPLICATION_OPERATOR:
             answer = num1 * num2
-            break;
+            break
         case DIVISION_OPERATOR:
+            if (Number(num2) === 0) {
+                return MATH_ERROR
+            }
             answer = num1 / num2
-            break;
+            break
     }
+
     return `${Math.round(answer * 100) / 100}`
 }
 
-function highlightOperator(element) {
-    const currentHighlight = document.querySelector('.highlight')
-    if (currentHighlight) {
-        currentHighlight.classList.remove('highlight')
-        operator = null
+function populateDisplay(number) {
+    if (number === MATH_ERROR) {
+        display = number
+        displayEl.textContent = display
+        return
     }
-    if (element) {
-        element.classList.add('highlight')
-        operator = element.textContent
-    }
-}
 
-function displayNumber(number) {
-    let [integer, decimal = ''] = number.split('.')
+    let [integer, decimal] = number.split('.')
     integer = Number(integer).toLocaleString()
 
-    if (number.includes('.')) {
-        display = `${integer}.${decimal}`
-    } else {
-        display = integer
-    }
+    display = number.includes('.')
+        ? `${integer}.${decimal}`
+        : number
     displayEl.textContent = display
 }
 
-function appendDigit(operand, digit) {
-    const isDecimal = digit === '.'
-
-    if (operand === '0' && digit === '0') {
-        return '0'
-    } else if (isDecimal && operand.includes('.')) {
-        return operand
-    } else if (isDecimal && operand === '') {
-        return '0.'
-    } 
-    return operand += digit
-}
-
-function handleClickDigit(element) {
-    const digit = element.textContent
-
-    if (operator === null) {
-        operand1 = appendDigit(operand1, digit)
-        displayNumber(operand1)
-    } else if (operator === EQUALS_OPERATOR) {
-        operand1 = digit === '.' ? '0.' : digit
-        displayEl.textContent = operand1
-        highlightOperator()
-    } else if (ARITMETIC_OPERATIONS.includes(operator)) {
-        operand2 = appendDigit(operand2, digit)
-        displayNumber(operand2)
-    }
-}
-
-function handleClickOperator(element) {
-    if (element.textContent === EQUALS_OPERATOR && operand2 === '') {
-        return
-    } else if (operator === DIVISION_OPERATOR &&
-        operand2 !== '' &&
-        Number(operand2) === 0
-    ) {
-        display = MATH_ERROR
-        displayEl.textContent = display
-    } else if (operand2 !== '') {
-        const result = operate(operator, +operand1, +operand2)
-        operand1 = result
-        displayNumber(operand1)
-        operand2 = ''
-    }
-
-    highlightOperator(element)
-}
-
-function handleClickAllClear() {
-    display = '0'
+function allClear() {
     operand1 = '0'
-    operand2 = ''
-    highlightOperator()
-    displayEl.textContent = display
+    operand2 = null
+    operator = null
+    populateDisplay(operand1)
+    document.querySelector('.highlight')?.classList.remove('highlight')
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    displayEl.textContent = display
+    populateDisplay(operand1)
 })
 
-document.querySelector('.container').addEventListener('click', (e) => {
-    const elementClass = e.target.classList.value || ''
-
-    if (elementClass.includes('clear')) {
-        handleClickAllClear()
-    } else if (display === MATH_ERROR) {
+digitEls.forEach(el => el.addEventListener('click', (e) => {
+    if (display === MATH_ERROR) {
         return
-    } else if (elementClass.includes('digit')
-        || elementClass.includes('decimal')
+    }
+
+    const digit = e.target.textContent
+
+    // after calculating with "equals" operator
+    // if next button clicked is a digit
+    // clear the result and start a new calculation
+    if (operand1 &&
+        operator === EQUALS_OPERATOR &&
+        operand2 === null
     ) {
-        handleClickDigit(e.target)
-    } else if (elementClass.includes('operator')) {
-        handleClickOperator(e.target)
-    } 
+        allClear()
+    }
+    
+    if (operator === null) {
+        operand1 = operand1 === '0'
+            ? digit
+            : operand1 + digit
+        populateDisplay(operand1)
+    } else if (ARITHMETIC_OPERATOR.includes(operator)) {
+        operand2 = ['0', null].includes(operand2)
+            ? digit
+            : operand2 + digit
+        populateDisplay(operand2)
+    }
+}))
+
+operatorEls.forEach(el => el.addEventListener('click', (e) => {
+    if (display === MATH_ERROR) {
+        return
+    }
+
+    if (ARITHMETIC_OPERATOR.includes(operator) &&
+        operand2 !== null
+    ) {
+        operand1 = operate(operator, operand1, operand2)
+        operand2 = null
+        populateDisplay(operand1)  
+    }
+
+    operator = e.target.textContent
+    document.querySelector('.highlight')?.classList.remove('highlight')
+    e.target.classList.add('highlight')
+}))
+
+equalsEl.addEventListener('click', (e) => {
+    if (display === MATH_ERROR ||
+        operator === null ||
+        operand2 === null
+    ) {
+        return
+    }
+
+    document.querySelector('.highlight')?.classList.remove('highlight')
+    e.target.classList.add('highlight')
+
+    operand1 = operate(operator, operand1, operand2)
+    operand2 = null
+    operator = EQUALS_OPERATOR
+    populateDisplay(operand1)
 })
+
+decimalEl.addEventListener('click', (e) => {
+    if (operator === null && !operand1.includes('.')) {
+        operand1 += '.'
+        populateDisplay(operand1)
+    } else if (ARITHMETIC_OPERATOR.includes(operator) &&
+        (operand2 === null || !operand2.includes('.'))
+    ) {
+        operand2 = operand2 === null
+            ? '0.'
+            : operand2 + '.'
+        populateDisplay(operand2)
+    } else if (operator === EQUALS_OPERATOR) {
+        allClear()
+        operand1 = '0.'
+        populateDisplay(operand1)
+    }
+})
+
+allClearEl.addEventListener('click', allClear)
